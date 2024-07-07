f970056f2fa0a2209f954b7cedef30189cf04286
marco-scocca <marcoscocca1991@gmail.com>
2024-07-07 10:06:51 +0200

    istruzione che termina il ciclo

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
-const lista spesa = ['sapone', 'prosciutto', 'formaggio', 'pane'];
+const lista_spesa = ['sapone', 'prosciutto', 'formaggio', 'pane'];
 const ul = document.getElementById('lista-spesa');
 let i = 0;
-while (i < lista_spesa.lenght) {
+while (i < lista_spesa.length) {
     const li = document.createElement('li');
     li.innerText = lista_spesa[i];
     ul.append(li);
-
+    i++;
 
 }
