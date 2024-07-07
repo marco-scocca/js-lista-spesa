91a259a4cb700741d811bd82981a655cb6479460
marco-scocca <marcoscocca1991@gmail.com>
2024-07-07 09:46:57 +0200

    definisco while e creo list item

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
 const lista spesa = ['sapone', 'prosciutto', 'formaggio', 'pane'];
 const ul = document.getElementById('lista-spesa');
 let i = 0;
+while (i < lista_spesa.lenght) {
+    const li = document.createElement('li');
+
+}
